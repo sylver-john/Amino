132ab521387d8f9cc14a814462ea636601f45faf
Sylver Imhoff <sylverimhoff@gmail.com>
2017-12-28 17:58:01 +0100

    chore: remove hex-style css (not working)

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,29 +1,5 @@
 <template>
-  <div class="hexCrop">
-    <div class="hexContainer">
-      <div class="hex">L-Alanine</div>
-      <div class="hex">L-Arginine</div>
-      <div class="hex">L-Asparagine</div>
-      <div class="hex">L-Aspartate</div>
-      <div class="hex">L-Cystéine</div>
-      <div class="hex">L-Glutamate</div>
-      <div class="hex">L-Glutamine</div>
-      <div class="hex">Glycine</div>
-      <div class="hex">L-Histidine</div>
-      <div class="hex">L-Isoleucine</div>
-      <div class="hex">L-Leucine</div>
-      <div class="hex">L-Lysine</div>
-      <div class="hex">L-Méthionine</div>
-      <div class="hex">L-Phénylalanine</div>
-      <div class="hex">L-Proline</div>
-      <div class="hex">L-Pyrrolysine</div>
-      <div class="hex">L-Sélénocystéine</div>
-      <div class="hex">L-Sérine</div>
-      <div class="hex">L-Thréonine</div>
-      <div class="hex">L-Tryptophane</div>
-      <div class="hex">L-Tyrosine</div>
-      <div class="hex">L-Valine</div>       
-    </div>
+  <div> 
   </div>
 </template>
 
@@ -37,6 +13,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
