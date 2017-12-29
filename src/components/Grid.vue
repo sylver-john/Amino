9cc34c5ac705a9e2c4488ad517e3cd538457d179
Sylver Imhoff <sylverimhoff@gmail.com>
2017-12-29 17:07:16 +0100

    [WIP] feat: add style on profile component

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,45 +1,56 @@
 <template>
-  <div>
+  <div class="grid">
     <nav>
       <ul class="columns is-mobile">
-        <li class="column is-2 is-offset-2"><a href="#">L-Alanine</a></li>
-        <li class="column is-2"><a href="#">L-Arginine</a></li>
-        <li class="column is-2"><a href="#">L-Asparagine</a></li>
-        <li class="column"><a href="#">L-Aspartate</a></li>
+        <li class="column is-2 is-offset-2"><a href="#" @click="addProfileClass(1)">L-Alanine</a></li>
+        <li class="column is-2"><a href="#" @click="addProfileClass(2)">L-Arginine</a></li>
+        <li class="column is-2"><a href="#" @click="addProfileClass(3)">L-Asparagine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(4)">L-Aspartate</a></li>
       </ul>
       <ul class="columns is-mobile">
-        <li class="column"><a href="#">L-Cystéine</a></li>
-        <li class="column"><a href="#">L-Glutamate</a></li>
-        <li class="column"><a href="#">L-Glutamine</a></li>
-        <li class="column"><a href="#">Glycine</a></li>
-        <li class="column"><a href="#">L-Histidine</a></li>
-        <li class="column"><a href="#">L-Isoleucine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(5)">L-Cystéine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(6)">L-Glutamate</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(7)">L-Glutamine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(8)">Glycine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(9)">L-Histidine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(10)">L-Isoleucine</a></li>
       </ul>     
       <ul class="columns is-mobile">
-        <li class="column"><a href="#">L-Leucine</a></li>
-        <li class="column"><a href="#">L-Lysine</a></li>
-        <li class="column"><a href="#">L-Méthionine</a></li>
-        <li class="column"><a href="#">L-Phénylalanine</a></li>
-        <li class="column"><a href="#">L-Proline</a></li>
-        <li class="column"><a href="#">L-Pyrrolysine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(11)">L-Leucine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(12)">L-Lysine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(13)">L-Méthionine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(14)">L-Phénylalanine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(15)">L-Proline</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(16)">L-Pyrrolysine</a></li>
       </ul>
       <ul class="columns is-mobile">
-        <li class="column"><a href="#">L-Sélénocystéine</a></li>
-        <li class="column"><a href="#">L-Sérine</a></li>
-        <li class="column"><a href="#">L-Thréonine</a></li>
-        <li class="column"><a href="#">L-Tryptophane</a></li>
-        <li class="column"><a href="#">L-Tyrosine</a></li>
-        <li class="column"><a href="#">L-Valine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(17)">L-Sélénocystéine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(18)">L-Sérine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(19)">L-Thréonine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(20)">L-Tryptophane</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(21)">L-Tyrosine</a></li>
+        <li class="column"><a href="#" @click="addProfileClass(22)">L-Valine</a></li>
       </ul>
     </nav>
+    <profile :profileOpenClass="profileOpenClass" :profile="currentProfile"></profile>
   </div>
 </template>
-
 <script>
+import Profile from './Profile.vue'
+
 export default {
   name: 'Grid',
+  components: { Profile },
   data () {
     return {
+      profileOpenClass: false,
+      currentProfile: null
+    }
+  },
+  methods: {
+    addProfileClass (profile) {
+      this.profileOpenClass = !this.profileOpenClass
+      this.currentProfile = profile
     }
   }
 }
@@ -48,6 +59,10 @@ export default {
 * {
   margin: 0; padding: 0;
   font: 16px/1 Helvetica, Verdana, sans-serif;
+}
+
+.grid {
+  margin-top: 10%;
 }
 
 nav {
---
+++ src/components/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile" :class="[profileOpenClass ? 'profile-open' : '' ]">
+    <h2 class="title is-2">Title</h2>
+    <h3 class="title is-3">Description</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    <h3 class="title is-3">Usage</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    <h3 class="title is-3">Dosage</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    profileOpenClass: Boolean
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+.profile {
+  position: fixed;
+  top: 52px;
+  bottom: 0;
+  width: 500px;
+  right: -500px;
+  transition: 0.5s;
+  background-color: #a7a6a6;
+  padding: 30px;
+}
+.profile-open {
+  right: 0;
+}
+.profile p {
+  margin-bottom: 30px;
+}
+</style>
